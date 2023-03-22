4deefddb1627d255192b9fb4a1b9154668ac0071
AlexSokolov <sokolov.td@gmail.com>
2023-03-22 16:45:55 +0200

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '../styles/Home.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const a = 1;
   return (
     <>
       <Head>
@@ -115,9 +116,10 @@ export default function Home() {
               Instantly deploy your Next.js site to a shareable URL
               with&nbsp;Vercel.
             </p>
+            <p>Powered by ${a}</p>
           </a>
         </div>
       </main>
     </>
-  )
+  );
 }
